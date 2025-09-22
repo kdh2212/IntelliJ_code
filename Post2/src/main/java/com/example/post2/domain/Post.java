6640eaeb16d8cac0ae976553cef72468e40decc2
kdh2212 <jjun8248@gmail.com>
2025-09-22 19:06:52 +0900

    Spring : 게시판-페이징

---
+++ Post2/src/main/java/com/example/post2/domain/Post.java
@@ -0,0 +1,11 @@
+package com.example.post2.domain;
+
+import lombok.Data;
+
+@Data
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+    private String writer;
+}
