99ad50c81335a4bdce0e1d0437ba0ac13dc124af
kdh2212 <jjun8248@gmail.com>
2025-09-24 19:14:05 +0900

    Spring : Example Code ThymeLeaf로 변환

---
+++ Post0924/src/main/java/com/du/post0924/service/CommentService.java
@@ -0,0 +1,25 @@
+package com.du.post0924.service;
+
+
+
+import com.du.post0924.domain.Comment;
+import com.du.post0924.mapper.CommentMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentMapper commentMapper;
+
+    public List<Comment> getCommentbyPostId(Long postId){
+        return commentMapper.findbyPostId(postId);
+    }
+
+    public void addComment(Comment comment){
+        commentMapper.insert(comment);
+    }
+}
