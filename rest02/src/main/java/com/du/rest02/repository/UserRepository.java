73a4c01fbd99019a34f333a4c51df94e4c940821
kdh2212 <jjun8248@gmail.com>
2025-10-17 17:09:08 +0900

    Spring : Restful API

---
+++ rest02/src/main/java/com/du/rest02/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.du.rest02.repository;
+
+
+import com.du.rest02.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
