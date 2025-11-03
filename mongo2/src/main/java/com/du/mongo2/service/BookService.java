f07f13a43d396c4f4685ae36b67e7ef3659c6d3f
kdh2212 <jjun8248@gmail.com>
2025-11-03 16:12:35 +0900

    Mongo DB : Example Code

---
+++ mongo2/src/main/java/com/du/mongo2/service/BookService.java
@@ -0,0 +1,36 @@
+package com.du.mongo2.service;
+
+
+import com.du.mongo2.model.Book;
+import com.du.mongo2.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private final BookRepository repository;
+
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    public Optional<Book> getBookById(String id) {
+        return repository.findById(id);
+    }
+
+    public Book saveBook(Book book) {
+        return repository.save(book);
+    }
+
+    public void deleteBook(String id) {
+        repository.deleteById(id);
+    }
+}
+
